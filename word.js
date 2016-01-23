f383d229e43d64d3d3d063d079fbf112edafb824
Sean Conaty <anxiolydiot@Seans-MacBook-Pro.local>
2016-01-22 23:08:21 -0500

    writing the loops for checking arrays against user guesses

--- word.js
+++ word.js
@@ -1,11 +1,31 @@
 var Letter= require("./letter.js'";
-var Word = function(wrd){
-  this.word= wrd;
-  this.lets= [];
-  this.found= false;
-  this.getLets = function(){
-    for (i=0, i <= wrd.length, i++) {
-      this.lets[i] = new Object()}
+  var Word = function(wrd){
+    this.word= wrd;
+    this.lets= [];
+    this.found= false;
+    this.getLets = function(){
+      for (i=0; i <= wrd.length; i++) {
+        this.lets.push(new Letter(wrd[i])); 
 
-  }
-}
+
+    };
+  },
+  this.checkIfLetterFound = function (guessLetter){
+    var letterFound = 0;
+    var chars = this.lets[i];
+    for (i = 0; i <this.lets.length; i++){
+      if (chars.charac === guessLetter){
+          chars.appear === true;
+          letterFound ++;
+
+
+      }
+    }
+
+    return letterFound;
+    
+
+
+
+  
+    }
